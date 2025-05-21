fdd842da35ecf8de4b23d4193195a6cf02ee7497
Something Bad <nglykhov@gmail.com>
2025-05-21 11:40:26 +0300

    Add atlas example config and provider for gorm

---
+++ cmd/migrator/atlas-provider-gorm.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"ariga.io/atlas-provider-gorm/gormschema"
+
+	"auth/internal/database"
+)
+
+func main() {
+	stmts, err := gormschema.New("postgres").Load(
+		&database.User{},
+	)
+	if err != nil {
+		log.Fatalf("failed to load gorm schema: %v", err)
+	}
+	os.Stdout.WriteString(stmts)
+}
