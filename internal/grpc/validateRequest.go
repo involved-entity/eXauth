979e44cf636d96ccdf5051ade0ad9dd7ca23ad29
Something Bad <nglykhov@gmail.com>
2025-05-21 11:41:45 +0300

    Add validateRequest

---
+++ internal/grpc/validateRequest.go
@@ -0,0 +1,16 @@
+package grpc
+
+import (
+	"github.com/go-playground/validator/v10"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func ValidateRequest(rpb interface{}, dto interface{}) error {
+	validate := validator.New()
+	err := validate.Struct(dto)
+	if err != nil {
+		return status.Error(codes.InvalidArgument, err.Error())
+	}
+	return nil
+}
