b6de3c8a538e5894044eabea7566162feabd3475
Something Bad <karkadesnasvaem@gmail.com>
2025-05-25 21:14:09 +0300

    Add GetMe handler

--- internal/grpc/users/server.go
+++ internal/grpc/users/server.go
@@ -2,9 +2,13 @@ package users
 
 import (
 	"auth/api/users"
+	utils "auth/internal/grpc"
+	"auth/internal/pkg/database"
 	"context"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type usersAPI struct {
@@ -15,6 +19,38 @@ func Register(gRPC *grpc.Server) {
 	users.RegisterUsersServer(gRPC, &usersAPI{})
 }
 
+type GetMeDTO struct {
+	Token string `json:"token" validate:"required"`
+}
+
 func (s *usersAPI) GetMe(c context.Context, r *users.GetMeRequest) (*users.GetMeResponse, error) {
-	return nil, nil
+	dto := GetMeDTO{
+		Token: r.Token,
+	}
+
+	err := utils.ValidateRequest(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	rep := Repository{db: database.GetDB()}
+	id, err := utils.GetUserIDByJWT(dto.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := rep.GetUser(utils.UserInfo{ID: id})
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "user not found")
+	}
+
+	return &users.GetMeResponse{
+		User: &users.User{
+			Id:         int64(user.ID),
+			Username:   user.Username,
+			Email:      user.Email,
+			IsVerified: user.IsVerified,
+			IsAdmin:    user.IsAdmin,
+		},
+	}, nil
 }
